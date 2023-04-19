07debed8817b40c9f692a08b3d1879a04f01bc09
Jake Mckenzie-Edwards <jake.mckenzieedwards@gmail.com>
2023-04-19 10:37:48 +1000

    commit tree 30843dbe9de0c6f3ee1972619ebd0def1d1b7bae

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const main = document.getElementById("main");
+main.remove()
+
+const newHeader = document.createElement('h1')
+document.body.append(newHeader)
+
+newHeader.id = 'victory'
+newHeader.innerText = 'Jake is the champion'
